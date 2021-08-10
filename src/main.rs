947345cef729993303b3fb7116632a663b448dcc
Vasco Ferreira <vasco_mmf@hotmail.com>
2021-08-10 02:42:02 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,182 @@
+// States for the elemtns
+// use iced::button;
+// The elements to use
+// use iced::{Button, Column, Container, Element, Row, Text};
+// Styling options
+// use iced::{Align, HorizontalAlignment, Length};
+// The application settings
+// use iced::{executor, Application, Command, Settings};
+
+// Use a channel to keep data between threads
+use std::fs::File;
+use std::io::prelude::*;
+use std::sync::mpsc;
+
+use regex::Regex;
+
+mod queue;
+use queue::Queue;
+
+#[derive(Debug)]
+struct Video {
+    id: String,
+    title: String,
+    link: String,
+    format: String,
+    parts: u64
+}
+
+impl Video {
+    async fn new(url: &str) -> Video {
+        let re_video_info = Regex::new(r#"(<meta property="og:title" content="(?P<video_title>.*)"/>)[\n\d\w\W]*(file: '(?P<video_link>https://streaming-arquivo-ondemand.rtp.pt.*)(?P<video_index>index.*streams=)(?P<video_id>.*)(?P<video_format>\.mp4))"#).unwrap();
+
+        let resp = reqwest::get(url).await.unwrap();
+        let resp_body = resp.text().await.unwrap();
+
+        // Get the video name
+        let video_info = re_video_info.captures(&resp_body).unwrap();
+        
+        let index = video_info["video_index"].to_string();
+        let id = video_info["video_id"].to_string();
+        let title = video_info["video_title"].to_string();
+        let title = title.replace("&#8211;", "-");
+        
+        let link = video_info["video_link"].to_string();
+        let format = video_info["video_format"].to_string();
+
+        let video_index_link = format!("{}{}{}{}", link, index, id, format);
+
+        let re_video_index = Regex::new(r".*-(?P<last_part>.*)\.ts\n#EXT-X-ENDLIST").unwrap();
+        
+        let index_resp = reqwest::get(&video_index_link).await.unwrap();
+        let index_resp_body = index_resp.text().await.unwrap();
+        let parts = re_video_index.captures(&index_resp_body)
+            .unwrap()["last_part"].to_string()
+            .parse::<u64>().unwrap();
+        
+        Video {
+            id,
+            title,
+            link,
+            format,
+            parts
+        }       
+    }
+    
+    fn get_part_download_link(&self, part: u64) -> String {
+        format!("{}{}-{}.ts", self.link, self.id, part)
+    }
+
+    async fn fetch_video_info(url: &str, tx: mpsc::Sender<Video>) {
+        let video = Video::new(url).await;
+        tx.send(video).unwrap();
+    }
+}
+
+async fn download_video(video: Video) {
+    // Create a video file
+    let file_name = format!("./downloads/{}.ts", video.title);
+    let mut file = File::create(file_name).unwrap();
+
+    let client = reqwest::Client::new();
+
+    for part in 1..=video.parts {
+        // Add the part number placement
+        let link = video.get_part_download_link(part);
+
+        // Fetch the video as bytes
+        let resp = client.get(&link).send().await;
+        match resp {
+            Ok(r) => {
+                match r.status() {
+                    reqwest::StatusCode::OK => {
+                        let video = r.bytes().await.unwrap();
+                        let _ = file.write(video.as_ref()).unwrap();
+                    }
+                    _ => {
+                        break;
+                    }
+                }
+            }
+            _ => {
+                break;
+            }
+        }
+        println!("Downloading {}: {:.2}%", part, part as f64 / video.parts as f64);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    
+    let (tx, rx) = mpsc::channel();
+    let max_work_async: usize = 3;
+
+    let video_queue = Queue::new();
+    video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-01/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-02/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-03/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-04/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-05/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-06/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-07/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-08/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-09/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-10/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-11/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-12/");
+    /video_queue.add_work("https://arquivos.rtp.pt/conteudos/o-processo-dos-tavoras-episodio-13/");
+
+    while video_queue.length().unwrap() > 0 {
+        let mut current_work = 0;
+        let mut tasks = Vec::new();
+        while current_work < max_work_async {
+            if let Some(link) = video_queue.get_work() {
+                let tx_clone = tx.clone();
+                let task = tokio::spawn(Video::fetch_video_info(link, tx_clone));
+                tasks.push(task);
+                current_work += 1;
+            } else {
+                break;
+            }
+        }
+        
+        // Wait for all the threads to close
+        for task in tasks {
+            task.await.unwrap();
+        }
+
+        current_work = 0;
+    }
+    
+    // Drop tx as it is no longer needed
+    // This will allow the channel to close
+    // And get all the data in the rx.
+    drop(tx);
+
+    let download_queue = Queue::new();
+    for video in rx {
+        download_queue.add_work(video).unwrap();
+    }
+
+    while download_queue.length().unwrap() > 0 {
+        let mut current_work = 0;
+        let mut tasks = Vec::new();
+        while current_work < max_work_async {
+            if let Some(video) = download_queue.get_work() {
+                let task = tokio::spawn(download_video(video));
+                tasks.push(task);
+                current_work += 1;
+            } else {
+                break;
+            }
+        }
+        
+        // Wait for all the threads to close
+        for task in tasks {
+            task.await.unwrap();
+        }
+        current_work = 0;
+    }
+
+}
---
+++ src/queue.rs
@@ -0,0 +1,86 @@
+use std::sync::{Arc, Mutex};
+use std::collections::VecDeque;
+
+/// Create a WorkQueue of any type that holds all the work to be done
+///
+/// # Example
+///
+/// ```
+/// let max_work_async: usize = 3;
+/// 
+/// // Check if there is work to be done
+/// while queue_clone.length().unwrap() > 0 {
+///     let mut tasks = Vec::new();
+///     let mut current_work: i32 = 0;
+///
+///     // Don't overwork your PC. Set a maximum number of tasks async
+///     while current_work < max_work_async {
+///         if let Some(work) = queue_clone.get_work() {
+///             let task = tokio::task::spawn(complex_calculation(work, 1000));
+///             tasks.push(task);
+///             current_work += 1;
+///         } else {
+///             break;
+///         }
+///     }
+/// 
+///     for task in tasks {
+///         let result = task.await.unwrap();
+///         tx_clone.send(result).unwrap();
+///     }
+/// 
+///     current_work = 0;
+/// }
+/// ```
+#[derive(Debug, Clone)]
+pub struct Queue<T> {
+    queue: Arc<Mutex<VecDeque<T>>>,
+}
+
+impl<T> Queue<T> {
+    /// Create a new empty queue
+    pub fn new() -> Self {
+        Self {
+            queue: Arc::new(Mutex::new(VecDeque::new())),
+        }
+    }
+
+    /// Add work to the queue
+    pub fn add_work(&self, work: T) -> Result<(), ()> {
+        let queue = self.queue.lock();
+
+        if let Ok(mut q) = queue {
+            q.push_back(work);
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    /// Get the first available work
+    pub fn get_work(&self) -> Option<T> {
+        // Lock the queue to fetch a work to do and prevent other threads from
+        // fetching the same work.
+        let queue = self.queue.lock();
+
+        if let Ok(mut q) = queue {
+            // Remove the first work available
+            // Follows the the FIFO layout
+            q.pop_front()
+        } else {
+            None
+        }
+    }
+
+    /// Count the work left
+    pub fn length(&self) -> Option<usize> {
+        let queue = self.queue.lock();
+
+        if let Ok(q) = queue {
+            Some(q.len())
+        } else {
+            None
+        }
+    }
+}
+
